3e91ba072980c291cd8ff532eba6598928e78d82
CruiseDevice <achavan1211@gmail.com>
2025-01-11 14:54:24 -0800

    Fix type in RegisterForm component

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -90,7 +90,7 @@ export default function RegisterForm () {
                 required
                 placeholder="Email address"
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                value={FormData.email}
+                value={formData.email}
                 onChange={handleChange}/>
             </div>
             <div>
