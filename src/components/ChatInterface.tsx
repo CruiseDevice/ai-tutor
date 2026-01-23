deaef55fbfb46b750a3ea6ac8b8647145b89c224
CruiseDevice <achavan1211@gmail.com>
2026-01-23 11:38:14 -0800

    refactor: polish mobile styles and toggle switch centering

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -537,7 +537,7 @@ export default function ChatInterface({
                 onChange={(e) => setUseAgent(e.target.checked)}
                 className="sr-only peer"
               />
-              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-1/2 after:-translate-y-1/2 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
             </label>
             <div className="flex flex-col">
               <span className="text-xs font-medium text-gray-700">
